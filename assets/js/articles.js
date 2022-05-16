3ec1743a1f9b45b488461715e8cf9ffed4c79490
Guillaume David <guillaumedaavidpro@gmail.com>
2022-05-16 10:44:42 +0200

    list article

--- assets/js/articles.js
+++ assets/js/articles.js
@@ -1,5 +1,21 @@
 fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@guillaumedaavid")
 .then((resp) => resp.json())
 .then(function(response){
-  console.log(response)
+  for (let i = 0; i<response.items.length; i++){
+    const articles = document.getElementById('articles');
+    const block = document.createElement('div');
+    const newA = document.createElement('a');
+    const image = document.createElement('img');
+    image.classList.add('img_block-articles');
+    image.src = response.items[i].thumbnail;
+    newA.textContent = response.items[i].title;
+    newA.href = response.items[i].link;
+    block.classList.add('block_articles');
+    const newTitle = document.createElement('h3');
+    newTitle.appendChild(newA);
+    block.appendChild(image);
+    block.appendChild(newTitle);
+    articles.appendChild(block);
+  };
+  console.log(response.items)
 });
