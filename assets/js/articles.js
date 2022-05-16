745c36648b9cc457382d7c6d1140aa94c4977fcd
Guillaume David <guillaumedaavidpro@gmail.com>
2022-05-16 11:29:35 +0200

    styles articles

--- assets/js/articles.js
+++ assets/js/articles.js
@@ -10,6 +10,8 @@ fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@gui
     image.src = response.items[i].thumbnail;
     newA.textContent = response.items[i].title;
     newA.href = response.items[i].link;
+    newA.target = "_blank";
+    newA.classList.add('link_article')
     block.classList.add('block_articles');
     const newTitle = document.createElement('h3');
     newTitle.appendChild(newA);
